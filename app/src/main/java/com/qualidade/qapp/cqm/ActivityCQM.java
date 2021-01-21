ba8455ffbb66a880a1c7ee9892750982253538dc
duhspbr <42584849+duhspbr@users.noreply.github.com>
2021-01-21 09:01:14 -0300

    Update project structure

---
+++ app/src/main/java/com/qualidade/qapp/cqm/ActivityCQM.java
@@ -0,0 +1,16 @@
+package com.qualidade.qapp.cqm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.qualidade.qapp.R;
+
+public class ActivityCQM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c_q_m);
+    }
+}
